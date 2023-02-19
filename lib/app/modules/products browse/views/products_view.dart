61b89705ed4ff82d077fcb2d9a6fb76e32128621
MD. Mahmudul Hasan <mahmudul.ri@gmail.com>
2023-02-19 15:15:36 +0600

    added product screen

--- lib/app/modules/products browse/views/products_view.dart
+++ lib/app/modules/products browse/views/products_view.dart
@@ -27,22 +27,22 @@ class _ProductsViewState extends State<ProductsView> {
           ),
         ),
         centerTitle: true,
-        actions: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Icon(
-              Icons.search,
-              color: Colors.white,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Icon(
-              Icons.notifications_none_outlined,
-              color: Colors.white,
-            ),
-          )
-        ],
+        // actions: [
+        //   Padding(
+        //     padding: const EdgeInsets.all(8.0),
+        //     child: Icon(
+        //       Icons.search,
+        //       color: Colors.white,
+        //     ),
+        //   ),
+        //   Padding(
+        //     padding: const EdgeInsets.all(8.0),
+        //     child: Icon(
+        //       Icons.notifications_none_outlined,
+        //       color: Colors.white,
+        //     ),
+        //   )
+        // ],
       ),
       body: Column(
         children: [
@@ -52,37 +52,37 @@ class _ProductsViewState extends State<ProductsView> {
               color: Colors.white30,
               child: Column(
                 children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Container(
-                      height: 60,
-                      decoration: BoxDecoration(
-                          border: Border.all(
-                        width: 1,
-                        color: Colors.grey,
-                      )),
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: 20,
-                        ),
-                        child: Row(
-                          children: [
-                            Text(
-                              "Cocola Noodles",
-                              style: TextStyle(
-                                fontSize: 22,
-                              ),
-                            ),
-                            Spacer(),
-                            Icon(
-                              Icons.search,
-                              size: 40,
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
+                  // Padding(
+                  //   padding: const EdgeInsets.all(8.0),
+                  //   child: Container(
+                  //     height: 60,
+                  //     decoration: BoxDecoration(
+                  //         border: Border.all(
+                  //       width: 1,
+                  //       color: Colors.grey,
+                  //     )),
+                  //     child: Padding(
+                  //       padding: const EdgeInsets.symmetric(
+                  //         horizontal: 20,
+                  //       ),
+                  //       child: Row(
+                  //         children: [
+                  //           Text(
+                  //             "Cocola Noodles",
+                  //             style: TextStyle(
+                  //               fontSize: 22,
+                  //             ),
+                  //           ),
+                  //           Spacer(),
+                  //           Icon(
+                  //             Icons.search,
+                  //             size: 40,
+                  //           ),
+                  //         ],
+                  //       ),
+                  //     ),
+                  //   ),
+                  // ),
                   myproductBox("Cocola mini pack"),
                 ],
               ),
@@ -203,83 +203,102 @@ class _ProductsViewState extends State<ProductsView> {
   Widget myproductBox(String productName) {
     var screenheight = MediaQuery.of(context).size.height;
     var screenWidth = MediaQuery.of(context).size.width;
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Container(
-        padding: EdgeInsets.only(
-          left: 10,
-          right: 30,
+
+    return Column(
+      children: [
+        SizedBox(
+          height: screenheight * 0.030,
         ),
-        height: screenheight * 0.050,
-        decoration: BoxDecoration(
-          color: Colors.grey,
-        ),
-        child: Row(
-          children: [
-            Text(
-              productName,
-              style: TextStyle(
-                fontSize: screenheight * 0.022,
-              ),
-            ),
-            Spacer(),
-            Container(
-              width: screenWidth * 0.20,
-              color: Colors.white,
-              child: Center(
-                child: Text(
-                  counter.toString(),
+        Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            color: Colors.blueGrey,
+          ),
+          height: screenheight * 0.300,
+          width: screenWidth - 50,
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  productName.toString(),
                   style: TextStyle(
-                    fontSize: screenheight * 0.022,
+                    fontSize: 22,
+                    color: Colors.white,
+                  ),
+                ),
+                Text(
+                  "120.40",
+                  style: TextStyle(
+                    fontSize: 22,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        SizedBox(
+          height: screenheight * 0.030,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            InkWell(
+              onTap: () {
+                setState(() {
+                  counter--;
+                });
+              },
+              child: Container(
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.grey,
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Icon(
+                    Icons.remove,
+                    size: screenheight * 0.050,
                   ),
                 ),
               ),
             ),
             SizedBox(
-              width: screenWidth * 0.10,
+              width: screenWidth * 0.040,
             ),
-            Container(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Row(
-                    children: [
-                      Container(
-                        height: 30,
-                        width: 30,
-                        decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          color: Colors.white,
-                        ),
-                        child: Icon(Icons.remove),
-                      ),
-                      SizedBox(
-                        width: 5,
-                      ),
-                      InkWell(
-                        onTap: () {
-                          setState(() {
-                            counter++;
-                          });
-                        },
-                        child: Container(
-                          height: 30,
-                          width: 30,
-                          decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            color: Colors.white,
-                          ),
-                          child: Icon(Icons.add),
-                        ),
-                      ),
-                    ],
-                  )
-                ],
+            Text(
+              counter.toString(),
+              style: TextStyle(
+                fontSize: screenheight * 0.030,
+              ),
+            ),
+            SizedBox(
+              width: screenWidth * 0.040,
+            ),
+            InkWell(
+              onTap: () {
+                setState(() {
+                  counter++;
+                });
+              },
+              child: Container(
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.grey,
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Icon(
+                    Icons.add,
+                    size: screenheight * 0.050,
+                  ),
+                ),
               ),
             ),
           ],
         ),
-      ),
+      ],
     );
   }
 }
